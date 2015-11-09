f6b30e25cddfa9f3a553657fcebddf1a6be186b6
gio <=>
2015-11-10 02:08:09 +0400

    enquire user page show contact persons

--- controller/custom/user.php
+++ controller/custom/user.php
@@ -107,7 +107,14 @@ class user extends connection{
 				`studio404_users`.`mobile`, 
 				`studio404_users`.`office_phone`, 
 				`studio404_users`.`email`, 
-				`studio404_users`.`web_address` 
+				`studio404_users`.`web_address`, 
+				`studio404_users`.`contact_person`, 
+				`studio404_users`.`ad_position1`, 
+				`studio404_users`.`ad_email1`, 
+				`studio404_users`.`ad_person2`, 
+				`studio404_users`.`ad_position2`, 
+				`studio404_users`.`ad_mobile2`, 
+				`studio404_users`.`ad_email2` 
 				';	
 			}else if(Input::method("GET","t")=="individual"){
 				$columns = '
